cf5ca989a0dfe444f8f33d4009962fecaf65af8c
Hugues R <hugues.rabier@gmail.com>
2026-03-08 15:33:45 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8930125250-d5b7e",
+  "appId": "1:504325434909:web:584f3c50cdbbc4a3a0e0f4",
+  "apiKey": "AIzaSyBFLfQVEH910mr_1QmIgPqufFQkYlaRBbc",
+  "authDomain": "studio-8930125250-d5b7e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "504325434909"
+};
