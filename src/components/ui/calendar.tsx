86503c1fee35cc2f9186e8c2e6f9a330a5691ebf
Hugues R <hugues.rabier@gmail.com>
2026-03-09 14:49:40 +0000

    Aucun changement, ces fleches demeurent noires

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -27,7 +27,7 @@ function Calendar({
         nav: "space-x-1 flex items-center",
         nav_button: cn(
           buttonVariants({ variant: "outline" }),
-          "h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100"
+          "h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100 text-foreground"
         ),
         nav_button_previous: "absolute left-1",
         nav_button_next: "absolute right-1",
@@ -54,8 +54,8 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4 text-foreground" />,
-        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4 text-foreground" />,
+        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
+        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
       }}
       {...props}
     />
